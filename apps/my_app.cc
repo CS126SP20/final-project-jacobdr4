fcfaa3ba9f785787aae0512c72d733d060100ad8
jacobrubin <jacobdr4@illinois.edu>
2020-04-24 14:03:09 -0500

    Added the Coc-Core, but there are still issues with compiling my code

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -1,26 +1,42 @@
 // Copyright (c) 2020 [Your Name]. All rights reserved.
 
 #include "my_app.h"
-
 #include <cinder/app/App.h>
-#include <cocButton.h>
+#include <ciButton.h>
 
 namespace myapp {
 
 using cinder::app::KeyEvent;
 
-MyApp::MyApp() { }
+MyApp::MyApp() {}
 
 void MyApp::setup() {
-
+  SetUpButtons();
 }
 
-void MyApp::update() { }
+void MyApp::update() {}
 
-void MyApp::draw() {
+void MyApp::draw() {}
 
+void MyApp::keyDown(KeyEvent event) {}
+
+void MyApp::SetButtonCoordinates() {
+  int screen_width = cinder::app::getWindowWidth();
+
+  for (int x = 0; x < kNumButtons; x++) {
+    button_x.push_back(((screen_width / kNumButtons + 2) * x) +
+                       (screen_width / kNumButtons + 2));
+  }
 }
 
-void MyApp::keyDown(KeyEvent event) { }
+void MyApp::SetUpButtons() {
+  SetButtonCoordinates();
+  int button_size = 50;
+  line_button.setRect(button_x[0], button_size, button_x[0] + button_size, button_size * 2);
+  erase_button.setRect(button_x[1], button_size, button_x[1] + button_size, button_size * 2);
+  fill_button.setRect(button_x[2], button_size, button_x[2] + button_size, button_size * 2);
+  draw_button.setRect(button_x[3], button_size, button_x[3] + button_size, button_size * 2);
+}
 
-}  // namespace myapp
+}
+// namespace myapp
--- apps/my_app.h
+++ apps/my_app.h
@@ -2,13 +2,31 @@
 
 #ifndef FINALPROJECT_APPS_MYAPP_H_
 #define FINALPROJECT_APPS_MYAPP_H_
-
+#include <cocButton.h>
+#include "cinder/app/RendererGl.h"
+#include "cinder/gl/gl.h"
+#include "cinder/Utilities.h"
 #include <cinder/app/App.h>
+#include <vector>
 
-
+using std::vector;
 namespace myapp {
 
 class MyApp : public cinder::app::App {
+ private:
+  const int kNumButtons = 4;
+  vector<int> button_x;
+
+  coc::ciButton line_button;
+  coc::ciButton fill_button;
+  coc::ciButton erase_button;
+  coc::ciButton draw_button;
+
+
+  void SetButtonCoordinates();
+  void SetUpButtons();
+
+
  public:
   MyApp();
   void setup() override;
